d7a885a5322fcea8dc88e62844cee0dc78b64991
xuedongZ <xuedong6624@gmail.com>
2019-03-29 15:52:10 +0800

    adjust /config/index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    context:[ //代理路径
+      '/shopping',
+      '/v1',
+      '/v2',
+      '/v3',
+      '/v4',
+    ],
+    proxypath: 'https://mainsite-restapi.ele.me',
     proxyTable: {},
+    
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -33,17 +42,17 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true
+    cssSourceMap: false
   },
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../elm/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../elm'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/elm/',
 
     /**
      * Source Maps
