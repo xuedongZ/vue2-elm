54cf52a3cad93847e71477e5cc9c824367b243bd
xuedongZ <xuedong6624@gmail.com>
2019-03-29 16:27:59 +0800

    c main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,58 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import store from './store/'
+import ajax from './config/ajax'
+import fetch from './config/fetch'
+import './style/common'
+import './config/rem'
 
-Vue.config.productionTip = false
+// ajax('GET', '/shopping/restaurants',{
+// 	latitude: 31.16407, 
+// 	longitude: 121.38876, 
+// 	offset: 0, 
+// 	limit: 20, 
+// 	'extras[]': 'activities',
+// })
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+
+fetch('get', '/v2/index_entry', {
+	geohash: 'wtw3630xg5e', 
+	group_type: 1, 
+	'flags[]': 'F',
+}).then(res => {
+	console.log(res)
+}).catch(error => {
+	console.log(error)
 })
+
+
+// var obj = {
+// 	mobile: '13681711254', 
+// 	scene: 'login', 
+// 	type: 'sms',
+// }
+
+// var a = JSON.stringify(obj)
+// ajax('POST', '/v4/mobile/verify_code/send', a)
+
+
+// var loginObj = {
+// 	code:212128,
+// 	mobile: '13681711254',
+// 	validate_token: '0af8df2bfd363e37cba0c616498e049777d7b118f13be41664491a05d8ad9da2'
+// }
+// var b = JSON.stringify(loginObj)
+
+// ajax('POST', '/v1/login/app_mobile', b)
+
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+	routes
+})
+
+new Vue({
+	router,
+	store,
+}).$mount('#app')
