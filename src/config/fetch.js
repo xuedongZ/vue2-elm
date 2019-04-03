fd6df68237da0b44590267f235d892e7a64ff36a
xuedongZ <xuedong6624@gmail.com>
2019-04-03 13:24:42 +0800

    update

--- src/config/fetch.js
+++ src/config/fetch.js
@@ -1,33 +1,39 @@
-let baseUrl = ''
+import {baseUrl} from './env'
 
-export default (type='GET', url='', data={}) => {
-	return new Promise((resolve, reject) => { //定义一个promise
+export default (type = 'GET', url = '', data = {}) => {
+	return new Promise((resolve, reject) => { //返回一个promise
 		type = type.toUpperCase();
-
-		let dataStr = ''; //数据拼接字符串
-		Object.keys(data).forEach( key => {
-			dataStr += key + '=' + data[key] + '&';
-		})
-		dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
-
-		if (dataStr !== '') {
-			url = baseUrl + url + '?' + dataStr;
-		}else {
-			url = baseUrl + url
-		}
-		
-		fetch(url, {
+		url = baseUrl + url
+		let requestObj = {
+			credentials: 'include',
 		  	method: type,
 		  	headers: {
 		      	'Accept': 'application/json',
       			'Content-Type': 'application/json'
 		  	},
 		  	mode: "no-cors",
-		  	// body: JSON.stringify({
-		   //    	content: "留言内容"
-		  	// })
-		}).then(res => {
-		  	if (res.status === 200) {
+		}
+
+		if (type == 'GET') {
+			let dataStr = ''; //数据拼接字符串
+			Object.keys(data).forEach(key => {
+				dataStr += key + '=' + data[key] + '&';
+			})
+
+			if (dataStr !== '') {
+				dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
+				url = url + '?' + dataStr;
+			}
+		}else if (type == 'POST') {
+			Object.defineProperty(requestObj, 'body', {
+				value: JSON.stringify(data)
+			})
+		}else {
+			reject('error type')
+		}
+		
+		fetch(url, requestObj).then(res => {
+		  	if (res.status == 200) {
 		      	return res.json()
 		  	} else {
 		      	return reject(res)
