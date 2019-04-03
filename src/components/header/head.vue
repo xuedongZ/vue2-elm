79bbd20e4f4deacb39b6c7da972021cd4d3bf2c6
xuedongZ <xuedong6624@gmail.com>
2019-04-04 00:14:46 +0800

    upadate

--- src/components/header/head.vue
+++ src/components/header/head.vue
@@ -1,53 +1,52 @@
 <template>
-    <header id='head_top'>
-        <slot name='logo'></slot>
-        <router-link to='/login' v-if='signinUp' class="head_login">登陆|注册</router-link>
-    </header>
+  <header id="head_top">
+    <slot name="logo"></slot>
+    <div class="head_goback" v-if="goBack" @click="$router.go(-1)"></div>
+    <router-link to="/login" v-if="signinUp" class="head_login">登陆|注册</router-link>
+    <div class="title_head" v-if="headTitle">{{headTitle}}</div>
+    <slot name="changecity"></slot>
+  </header>
 </template>
 
 <script>
-
 export default {
-	data(){
-            return{
-                
-            }
-        },
-        created(){
-
-        },
-        mounted(){
-
-        },
-        props: ['signinUp'],
-        computed: {
-            
-        },
-        methods: {
-
-        },
-
-}
-
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  props: ["signinUp", "headTitle", "goBack"],
+  computed: {},
+  methods: {}
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../style/mixin.scss';
+@import "../../style/mixin.scss";
 
-    #head_top{
-        height: 1.95rem;
-        width: 100%;
-        background-color: $blue;
-        position: fixed;
-        left: 0;
-        top: 0;
-    }
-    .head_login{
-        position: absolute;
-        font-size: 0.6rem;
-        color: #fff;
-        right: 0.55rem;
-        top: 50%;
-        transform: translateY(-50%);
-    }
+#head_top {
+  height: 1.95rem;
+  width: 100%;
+  background-color: $blue;
+  position: fixed;
+  left: 0;
+  top: 0;
+  @include wh(100%, 1.95rem);
+}
+.head_goback {
+  left: 0.4rem;
+  @include wh(0.375rem, 0.7rem);
+  @include center-top;
+  @include backimg("../../images/icon_back.png");
+}
+.head_login {
+  right: 0.55rem;
+  top: 50%;
+  @include sizecolor(0.6rem, #fff);
+  @include center-top;
+}
+.title_head {
+  @include sizecolor(0.7rem, #fff);
+  @include center;
+}
 </style>
--- src/service/getData.js
+++ src/service/getData.js
@@ -1,9 +1,22 @@
 import fetch from '../config/fetch'
+import * as home from './tempdata/home'
+
 
 /**
  * 获取首页默认地址
  */
-const cityGuess = () => fetch('GET', '/v1/cities', {type: 'guess'})
+// const cityGuess = () => fetch('GET', '/v1/cities', {type: 'guess'})
+
+const setpromise = data => {
+	return new Promise((resolve, reject) => {
+		resolve(data)
+	})
+}
+
+const cityGuess = () => setpromise(home.guesscity)
+const hotcity = () => setpromise(home.hotcity)
+const groupcity = () => setpromise(home.groupcity)
 
 
-export {cityGuess}
+export {cityGuess, hotcity, groupcity}
+
