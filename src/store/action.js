ff55ce5df20c75e42c2d02f29091ff05226a203b
xuedongZ <xuedong6624@gmail.com>
2019-04-08 19:07:39 +0800

    updata cart

--- src/store/action.js
+++ src/store/action.js
@@ -1,10 +1,10 @@
 import { cityGuess } from '../service/getData'
-import { GET_DATA2 } from './mutation-types.js'
+import { GET_DATA } from './mutation-types.js'
 
 export default {
   getData({ commit, state }) {
     cityGuess().then(res => {
-      commit(GET_DATA2, { data: res })
+      commit(GET_DATA, { data: res })
     })
   }
 }
