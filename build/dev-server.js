742b73df1eae28838cb8f667f80fde7a0efd9617
xuedongZ <xuedong6624@gmail.com>
2019-04-08 11:30:27 +0800

    updata

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,4 +1,3 @@
-
 var config = require('../config')
 if (!process.env.NODE_ENV) process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 var path = require('path')
@@ -10,40 +9,40 @@ var webpackConfig = require('./webpack.dev.conf')
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port
-    // Define HTTP proxies to your custom API backend
-    // https://github.com/chimurai/http-proxy-middleware
+// Define HTTP proxies to your custom API backend
+// https://github.com/chimurai/http-proxy-middleware
 
 var server = express()
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-        colors: true,
-        chunks: false
-    }
+  publicPath: webpackConfig.output.publicPath,
+  stats: {
+    colors: true,
+    chunks: false
+  }
 })
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
-    // force page reload when html-webpack-plugin template changes
+// force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function(compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
-        hotMiddleware.publish({
-            action: 'reload'
-        })
-        cb()
+  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+    hotMiddleware.publish({
+      action: 'reload'
     })
+    cb()
+  })
 })
 
 var context = config.dev.context
 var proxypath = config.dev.proxypath
 
 var options = {
-    target: proxypath,
-    changeOrigin: true,
+  target: proxypath,
+  changeOrigin: true
 }
 if (context.length) {
-    server.use(proxyMiddleware(context, options))
+  server.use(proxyMiddleware(context, options))
 }
 
 // server.use(proxyMiddleware('/*/*', {
@@ -51,7 +50,6 @@ if (context.length) {
 //     changeOrigin: true,
 //     secure: false,
 // }))
-
 
 // handle fallback for HTML5 history API
 server.use(require('connect-history-api-fallback')())
@@ -68,15 +66,15 @@ var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsS
 server.use(staticPath, express.static('./static'))
 
 module.exports = server.listen(port, function(err) {
-    if (err) {
-        console.log(err)
-        return
-    }
-    var uri = 'http://localhost:' + port
-    console.log('Listening at ' + uri + '\n')
+  if (err) {
+    console.log(err)
+    return
+  }
+  var uri = 'http://localhost:' + port
+  console.log('Listening at ' + uri + '\n')
 
-    // when env is testing, don't need open it
-    if (process.env.NODE_ENV !== 'testing') {
-        //opn(uri)
-    }
+  // when env is testing, don't need open it
+  if (process.env.NODE_ENV !== 'testing') {
+    opn(uri)
+  }
 })
